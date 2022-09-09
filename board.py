6aa0f8c4da5a5df24687c0c7ebe5181b218c2bf2
FindingDorri <tho.le.nas@gmail.com>
2022-09-09 13:00:04 -0600

    Implemented tracktracking algorithm to solve any sodoku board

--- board.py
+++ board.py
@@ -1,14 +1,40 @@
+from turtle import back
+
+
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [8,0,0,0,0,0,0,0,0],
+    [0,0,3,6,0,0,0,0,0],
+    [0,7,0,0,9,0,2,0,0],
+    [0,5,0,0,0,7,0,0,0],
+    [0,0,0,0,4,5,7,0,0],
+    [0,0,0,1,0,0,0,3,0],
+    [0,0,1,0,0,0,0,6,8],
+    [0,0,8,5,0,0,0,1,0],
+    [0,9,0,0,0,0,4,0,0]
 ]
+
+
+def back_track(board):
+    empty_box = find_empty(board)
+
+    # if no empty box is found, the sudoku is sovled
+    if not empty_box:
+        return True
+    else:
+        row,col = empty_box
+
+    # loop all valid move (1-9)
+    for guess in range(1,10):
+        # check to see if guess is a valid solution
+        if is_valid(board,guess,(row,col)):
+            # add guess to board if valid
+            board[row][col]=guess
+            # recusrive call the backtrack algo to move on the the next empty box with a new valid move/guessed number  
+            if back_track(board):
+                return True
+            # if we loop through all the move and none of them is valid, we back track and set our last guess to 0
+            board[row][col] = 0
+    return False
 
 def print_board(board):
     for i in range(len(board)):
@@ -23,13 +49,18 @@ def print_board(board):
                 print(board[i][j])
             else:
                 print(str(board[i][j]) + " ", end="")
-        
-    
+
+
+
 def find_empty(board):
+    """Return the row and col index of an empty box"""
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i,j)
+    
+    # empty box is not found
+    return None
 
 def is_valid(board,guess,empty_position):
     # Check row
@@ -43,9 +74,22 @@ def is_valid(board,guess,empty_position):
         if board[i][empty_position[1]] == guess and i != empty_position[0]:
             return False
     
-    # Check box
+    # Check sub box
+    # if we divide the box by 3, we can find what sub box the box is in
     box_pos_x = empty_position[1] // 3
     box_pos_y = empty_position[0] // 3
 
-    pass
+    for i in range(box_pos_y * 3, box_pos_y * 3 +3):
+        for j in range(box_pos_x * 3, box_pos_x * 3 +3):
+            if board[i][j] == guess and (i,j) != empty_position:
+                return False
+
+    return True
+
+
+
+
+print_board(board)
+back_track(board)
+print("")
 print_board(board)
